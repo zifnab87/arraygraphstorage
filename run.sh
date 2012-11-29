f6fa51cec1b6d6850e94f6d342f5b9c703fd50be
Kostya Maleyev <kmaleyev@gmail.com>
2012-11-29 15:40:53 -0500

    Code to import/convert from CSV sparse edge-tuple representation into a scidb adjacency matrix (shell script issuing a sequence of iquery commands) and do something BFS-like with a predefined number of steps.
    Includes two sample small matrices in csv, one in abovementioned sparse
    format, another (same graph) in dense format (including "0-edges", N^2 rows
    total)
    
    NOTE: run.sh includes an absolute path to .scidb file, wich may need to
    be modified depending on the path project is checked out at!

---
+++ run.sh
@@ -0,0 +1,30 @@
+if [ "$1" == "" ]; then
+	echo "usage: ./setup.sh [BFS start vertex]"
+	exit
+fi
+echo Loading tuples representation into a flat array
+iquery -q 'create array adjFlat <vertex1:int64, vertex2:int64, isAdjacent:int64> [i=0:*,100000,0];'
+csv2scidb -p NNN -s 1 < input-tuples-sparse.csv > input.scidb
+iquery -q "load adjFlat from '/home/scidb/input.scidb'"
+rm input.scidb
+echo Converting into adjacency matrix
+iquery -q "create array adj <isAdjacent:int64> [vertex1=0:4,1000,0, vertex2=0:4,1000,0]"
+iquery -aq "redimension_store(adjFlat,adj);"
+iquery -q "drop array adjFlat;"
+echo Filling out NULL cells with 0\'s
+iquery -aq "merge(adj, build(adj,0))"
+#iquery -aq "store(build(<isAdjacent:int64>[i=0:0,1,0],0), subst_array)"
+#iquery -aq "store(substitute(adj, subst_array), adj)"
+#iquery -q "drop array subst_array;"
+echo Result:
+iquery -aq "show(adj)"
+iquery -aq "scan(adj)"
+echo Performing BFS
+iquery -q "create array stepV <isAdjacent:int64> [vertex1=0:1,1,0, vertex2=0:4,1000,0]"
+echo "stepV = "
+iquery -aq "subarray(adj,$1,0,$1,4)"
+echo "stepV * adj = "
+iquery -aq "multiply(multiply(subarray(adj,$1,0,$1,4),adj),adj)"
+echo "Cleaning up..."
+iquery -q "drop array adj;"
+iquery -q "drop array stepV;"
