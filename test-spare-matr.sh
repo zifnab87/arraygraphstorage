34de22e34ae8db7e0a10d11ace8de63ba0b0b6a2
Kostya Maleyev <kmaleyev@gmail.com>
2012-12-02 16:08:39 -0500

    matrix mult benchmark: scaling with size

--- test-spare-matr.sh
+++ test-spare-matr.sh
@@ -15,76 +15,6 @@ function AQL {
 	fi
 }
 
-function setup {
-	local P="1" # percent of non-zero element in a sparse matrix
-	local N=$1 
-	local CHNK1=$2
-	local CHNK2=$3
-	local Nm1=$(( $1 - 1 ))
-	echo Initializing test matrices...
-	AQL "create array A1 <val:int64> [i=0:$Nm1,$CHNK1,0, j=0:$Nm1,$CHNK2,0]"
-	AQL "create array B1 <val:int64> [i=0:$Nm1,$CHNK2,0, j=0:$Nm1,$CHNK1,0]"
-	AFL "store(build(A1,random() % 2),A1)"
-	AFL "store(build(B1,random() % 2),B1)"
-#iquery -aq "scan(A1)"
-#iquery -aq "scan(B1)"
-	AQL "create array A2 <val:int64> [i=0:$Nm1,$CHNK1,0, j=0:$Nm1,$CHNK2,0]"
-	AQL "create array B2 <val:int64> [i=0:$Nm1,$CHNK2,0, j=0:$Nm1,$CHNK1,0]"
-	AFL "store(build(A2, iif((random() % 1000) < $P,1,0)),A2)"
-	AFL "store(build(B2, iif((random() % 1000) < $P,1,0)),B2)"
-#iquery -aq "scan(A2)"
-#iquery -aq "scan(B2)"
-	AQL "create array A3 <val:int64> [i=0:$Nm1,$CHNK1,0, j=0:$Nm1,$CHNK2,0]"
-	AQL "create array B3 <val:int64> [i=0:$Nm1,$CHNK2,0, j=0:$Nm1,$CHNK1,0]"
-	local NNz=$(( $N * $N * $P / 1000 ))
-	local NNzm1=$(( $NNz -1 ))
-	# indirectly way of generating random sparse array
-	# WARN: possible cell collisions
-	# NOTE: number of non-zero elements in matrix always <= NNz
-	AQL "create array T <i:int64, j:int64, val:int64> [d=0:$NNzm1,$NNz,0];"
-	AFL "store(join(join(
-			build(<i:int64>[d=0:$NNzm1,$NNz,0],random() % $N),
-			build(<j:int64>[d=0:$NNzm1,$NNz,0],random() % $N)),
-			build(<val:int64>[d=0:$NNzm1,$NNz,0], 1)),
-		 T)"
-	AFL "redimension_store(T,A3);"
-	AFL "store(join(join(
-			build(<i:int64>[d=0:$NNzm1,$NNz,0],random() % $N),
-			build(<j:int64>[d=0:$NNzm1,$NNz,0],random() % $N)),
-			build(<val:int64>[d=0:$NNzm1,$NNz,0], 1)),
-		 T)"
-	AFL "redimension_store(T,B3);"
-	# straightforward way of generating random sparse array
-	# WARN: fails because of scidb bug...
-	#AFL "store(build_sparse(A3, 1, (random() % 1000) < $P),A3)"
-	#AFL "store(build_sparse(B3, 1, (random() % 1000) < $P),B3)"
-	#iquery -aq "scan(A3)"
-	#iquery -aq "scan(B3)"
-}
-
-function cleanup {
-	iquery -nq "drop array A1;" &>/dev/null
-	iquery -nq "drop array B1;" &>/dev/null
-	iquery -nq "drop array A2;" &>/dev/null
-	iquery -nq "drop array B2;" &>/dev/null
-	iquery -nq "drop array A3;" &>/dev/null
-	iquery -nq "drop array B3;" &>/dev/null
-	iquery -nq "drop array T;" &>/dev/null
-}
-
-if [ "$1" != "" ]; then
-	cleanup
-	# Recommended chunk size
-	if [ $1 -lt 1024 ]; then 
-		setup $1 $1 $1
-	else
-		setup $1 1024 1024
-	fi
-#	if [ $(( $1 * $1 )) -gt 10485760 ]; then
-#		# Max chunk size for 85 Mb segment
-#		#setup $1 "$(( 5 * 1024 ))" "$(( 2 * 1024 ))"
-#	fi
-fi
 
 function test {
     "$@" 
@@ -92,6 +22,7 @@ function test {
     	exit 1
     fi
 }	
+
 
 function time_msec {
 	_tm_out=$((time test "$@" >/dev/null) 2>&1)
@@ -105,17 +36,132 @@ function time_msec {
 	_tm_msec=$(expr ${_tm_sec#*.} + 0)
 	_tm_sec=$(expr ${_tm_sec%.*} + 0)
 	_tm_tmsec=$(( $_tm_min * 60 * 1000 + $_tm_sec * 1000 + $_tm_msec ))
-	echo $_tm_tmsec
+	echo "$_tm_tmsec"
 }
 
-echo "sparse (0's):"
-time_msec iquery -naq "count(multiply(A2,B2))"
-echo "sparse (empty cells, created w/ redimension() or build_sparse(), non-nullable value attribute):"
-time_msec iquery -naq "count(multiply(A3,B3))"
-echo "reference (naive native C code, in-memory):"
-gcc matmulttest.c -o tmp
-time_msec ./tmp $1
-rm -f tmp
-echo "dense:"
-time_msec iquery -naq "count(multiply(A1,B1))"
-#cleanup
+
+
+function trydrop {
+	iquery -nq "drop array $1;" &>/dev/null
+}
+
+function randmat {
+	# Recommended chunk size
+	local N=$1 
+	local TYPE=$2
+	local NAME=$3
+	local P=$4
+	if [ $# -lt 3 ]; then
+		echo "usage: randmat [N] [TYPE=dense|sparse-1|sparse-2] [NAME] [PROB_NONZERO]"
+		exit 1
+	fi
+	if [ $N -lt 1024 ]; then 
+		local CHNK1=$N
+		local CHNK2=$N
+	else
+		local CHNK1=1024
+		local CHNK2=1024
+	fi
+#	if [ $(( $1 * $1 )) -gt 10485760 ]; then
+#		# Max chunk size for 85 Mb segment
+#		#setup $1 "$(( 5 * 1024 ))" "$(( 2 * 1024 ))"
+#	fi
+	local P="1" # percent of non-zero element in a sparse matrix
+	local Nm1=$(( $1 - 1 ))
+	local NNz=$(( $N * $N * $P / 1000 ))
+	if [ $NNz -le 0 ]; then
+		local NNz=0
+	fi
+	local NNzm1=$(( $NNz -1 ))
+	if [ "$TYPE" == "dense" ]; then
+		AQL "create array $NAME <val:int64> [i=0:$Nm1,$CHNK1,0, j=0:$Nm1,$CHNK2,0]"
+		AFL "store(build($NAME,random() % 2),$NAME)"
+	else if [ "$TYPE" == "sparse-1" ]; then
+		AQL "create array $NAME <val:int64> [i=0:$Nm1,$CHNK1,0, j=0:$Nm1,$CHNK2,0]"
+		AFL "store(build($NAME, iif((random() % 1000) < $P,1,0)),$NAME)"
+	else if [ "$TYPE" == "sparse-2" ]; then
+		# indirectly way of generating random sparse array
+		# WARN: possible cell collisions
+		# NOTE: number of non-zero elements in matrix always <= NNz
+		AQL "create array $NAME <val:int64> [i=0:$Nm1,$CHNK1,0, j=0:$Nm1,$CHNK2,0]"
+		if [ $NNz -ge 1 ]; then
+			AQL "create array T <i:int64, j:int64, val:int64> [d=0:$NNzm1,$NNz,0];"
+			AFL "store(join(join(
+					build(<i:int64>[d=0:$NNzm1,$NNz,0],random() % $N),
+					build(<j:int64>[d=0:$NNzm1,$NNz,0],random() % $N)),
+					build(<val:int64>[d=0:$NNzm1,$NNz,0], 1)),
+				 T)"
+			AFL "redimension_store(T,$NAME);"
+		fi
+		iquery -nq "drop array T;" &>/dev/null
+		# straightforward way of generating random sparse array
+		# WARN: fails because of scidb bug...
+		#AFL "store(build_sparse($NAME, 1, (random() % 1000) < $P),$NAME)"
+	fi
+	fi
+	fi
+	#iquery -aq "scan($NAME)"
+}
+
+function cleanup {
+	trydrop A1
+	trydrop B1
+	trydrop A2
+	trydrop B2
+	trydrop A3
+	trydrop B3
+}
+
+
+function setup {
+	randmat $1 dense A1
+	randmat $1 dense B1
+	randmat $1 sparse-1 A2
+	randmat $1 sparse-1 B2
+	randmat $1 sparse-2 A3
+	randmat $1 sparse-2 B3
+}
+
+function timemult {
+	time_msec iquery -naq "count(multiply($1,$2))"
+}
+
+function benchmark {
+    if [ $# -ne 2 ]; then
+        echo "benchmark() expected 2 parameters"
+        exit 1
+    fi
+    local target=$1
+    local size=$2
+    local impl=${target%%-*}
+    local method=${target#*-}
+    echo "$target:$size"
+    if [ "$impl" == "scidb" ]; then
+        echo -en        "initializing..."
+	    randmat $size $method A
+    	randmat $size $method B
+        echo -en      "\rrunning...     "
+        local T=$(timemult A B)
+        echo -en      "\r               " "\r"
+    	trydrop A
+	    trydrop B
+    else if [ "$impl" == "native" ]; then
+        gcc -DNDEBUG -O3 matmulttest.c -o native
+        local T=$(time_msec ./native $method $size)
+        rm -f native
+    else
+        echo "target=[native-dense|native-sparse|scidb-dense|scidb-sparse-1|scidb-sparse=2]"
+        exit 1
+    fi
+    fi 
+    echo $T 
+    return 0
+}
+
+for size in 10 100 200 500 750
+do 
+    for target in 'native-dense' 'native-sparse' 'scidb-dense' 'scidb-sparse-1' 'scidb-sparse-2'
+    do
+        benchmark $target $size
+    done
+done
